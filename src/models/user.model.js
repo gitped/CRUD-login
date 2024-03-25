554c3546a8b12623668b318437b6d18b78f17a31
Pedro <gitpedcodes@gmail.com>
2024-03-25 14:06:27 -0500

    Project init with backend setup and token creation

---
+++ src/models/user.model.js
@@ -0,0 +1,25 @@
+// DB user data model
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+
+}, {
+  timestamps: true
+})
+
+export default mongoose.model('User', userSchema)
