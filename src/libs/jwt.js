44d61e7438c4bc65fd9e3baee80cafb585f63355
Pedro <gitpedcodes@gmail.com>
2024-03-25 17:45:49 -0500

    Create login with tokens

--- src/libs/jwt.js
+++ src/libs/jwt.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 import { TOKEN_SECRET } from "../config.js";
 
 export function createAccessToken(payload) {
-  new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     jwt.sign(
       payload,
       TOKEN_SECRET,
