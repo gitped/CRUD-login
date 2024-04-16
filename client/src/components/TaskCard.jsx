676fdb94ca23102128506db86be18ee4d3f6626d
Pedro <gitpedcodes@gmail.com>
2024-04-15 23:03:00 -0500

    Update the date format for tasks

--- client/src/components/TaskCard.jsx
+++ client/src/components/TaskCard.jsx
@@ -1,5 +1,8 @@
 import { Link } from 'react-router-dom';
 import { useTasks } from "../context/TaskContext.jsx";
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+dayjs.extend(utc);
 
 function TaskCard({ task }) {
   const { deleteTask } = useTasks();
@@ -9,12 +12,16 @@ function TaskCard({ task }) {
       <header className="flex justify-between">
         <h1 className="text-2xl font-bold">{task.title}</h1>
         <div className="flex gap-x-2 item-center">
-          <button onClick={() => {deleteTask(task._id)}}>Delete</button>
-          <Link to={`/tasks/${task._id}`}>Edit</Link>
+          <button onClick={() => {deleteTask(task._id)}}
+            className='bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-md'
+            >Delete</button>
+          <Link to={`/tasks/${task._id}`}
+            className='bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md'
+            >Edit</Link>
         </div>
       </header>
       <p className="text-slate-300">{task.description}</p>
-      <p>{new Date(task.date).toLocaleDateString()}</p>
+      <p>{dayjs(task.date).utc().format("MM/DD/YYYY")}</p>
     </div>
   )
 }
--- client/src/pages/TaskFormPage.jsx
+++ client/src/pages/TaskFormPage.jsx
@@ -2,6 +2,9 @@ import { useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { useNavigate, useParams } from 'react-router-dom';
 import { useTasks } from "../context/TaskContext.jsx";
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+dayjs.extend(utc);
 
 function TaskFormPage() {
   const { register, handleSubmit, setValue } = useForm();
@@ -15,32 +18,46 @@ function TaskFormPage() {
         const task = await getTask(params.id);
         setValue('title', task.title);
         setValue('description', task.description);
+        setValue('date', dayjs(task.date).utc().format("YYYY-MM-DD"));
       }
     };
     loadTask();
   }, []);
 
-  const onSubmit = handleSubmit(async (values) => {
-    if (params.id) await updateTask(params.id, values);
-    else createTask(values);
+  const onSubmit = handleSubmit(async (data) => {
+    const validData = {
+      ...data,
+      date: data.date ? dayjs.utc(data.date).format() : dayjs.utc().format()
+    };
+    if (params.id) await updateTask(params.id, validData);
+    else createTask(validData);
     navigate('/tasks');
   });
 
   return (
-    <div className="bg-zinc-800 max-w-md w-full p-10 rounded-md">
-      <form onSubmit={onSubmit}>
-        <input type="text" placeholder="title" autoFocus
+    <div className='flex h-screen items-center justify-center'>
+      <div className="bg-zinc-800 max-w-md w-full p-10 rounded-md">
+        <form onSubmit={onSubmit}>
+          <label htmlFor="title">Title</label>
+          <input type="text" placeholder="title" autoFocus
+            className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+            {...register("title")}
+          />
+          <label htmlFor="description">Description</label>
+          <textarea rows="3" placeholder="description"
+            className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+            {...register("description")}
+          ></textarea>
+          <label htmlFor="date">Date</label>
+          <input type="date"
           className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
-          {...register("title")}
-        />
-        <textarea rows="3" placeholder="description"
-          className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
-          {...register("description")}
-        ></textarea>
-        <button type="submit" className='bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 my-2 rounded'>
-          Save
-        </button>
-      </form>
+            {...register("date")}
+          ></input>
+          <button type="submit" className='bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 my-2 rounded'>
+            Save
+          </button>
+        </form>
+      </div>
     </div>
   )
 }
